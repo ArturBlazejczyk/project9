569a3b0605b093ec64ebc75690c8088883714f69
ArturBlazejczyk <34420486+ArturBlazejczyk@users.noreply.github.com>
2019-09-21 08:53:16 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const div = document.body.querySelector('div');
+let divX = 150;
+let divY = 50;
+div.style.left = `${divX}px`;
+div.style.top = `${divY}px`;
+
+let drawActive = false;
+
+let insertDivX;
+let insertDivY;
+
+div.addEventListener('mousedown', (e) => {
+    div.style.backgroundColor = 'gray';
+    drawActive = !drawActive;
+
+    insertDivX = e.offsetX;
+    insertDivY = e.offsetY;
+})
+
+div.addEventListener('mousemove', (e) => {
+    if (drawActive) {
+
+        divX = e.clientX - insertDivX;
+        divY = e.clientY - insertDivY;
+        div.style.left = `${divX}px`;
+        div.style.top = `${divY}px`;
+    }
+})
+
+div.addEventListener('mouseup', () => {
+    console.log('puszczone')
+    div.style.backgroundColor = 'black';
+    drawActive = !drawActive;
+})
